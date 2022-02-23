3843829e88e71dc11b6bc191c1fff257e5ff0e91
Таня <t@MacBook-Pro-Tana.local>
2022-02-23 20:06:41 +0200

    last commit

--- assets/main.js
+++ assets/main.js
@@ -9,161 +9,304 @@ console.log('Sample JavaScript #3 HW #17');
  * если нет – то счет продолжается
  */
 
-// console.log(counter()); // 0
+  var  counter = (function (){
+ var count = 0;
+ return function(num){
+     if (num !==undefined) count=num;
+     return count++;
+ };
+ }());
 
-// console.log(counter()); // 1
+  console.log(counter()); // 0
 
-// console.log(counter(100)); // 100
+  console.log(counter()); // 1
 
-// console.log(counter()); // 101
+   console.log(counter(100)); // 100
 
-// console.log(counter(500)); // 500
+   console.log(counter()); // 101
 
-// console.log(counter()); // 501
+   console.log(counter(500)); // 500
 
-// console.log(counter(0)); // 0
+   console.log(counter()); // 501
 
-// console.log(counter()); // 1
+   console.log(counter(0)); // 0
+
+   console.log(counter()); // 1
 
 /*
  * #2
  *
- * Создайте функцию counting, которая должна реализовать три метода с помощью замыкания:
+ * Создайте функцию counting, которая должна реализовать три метода
+ *  с помощью замыкания:
  * первоначальное значение счетчика – 0
  * counting.value() – возвращает значение счетчика
- * counting.value(n) – устанавливает значение счетчика, возвращает новое значение
+ * counting.value(n) – устанавливает значение счетчика, возвращает 
+ * новое значение
  * counting.increment() – увеличивает значение счетчика на 1
  * counting.decrement() – уменьшает значение счетчика на 1
  */
+ var counting = (function (){
+  var count = 0;
+ return { value(n){ 
+     if (n !== undefined) count=n;
+     return count;},
+     decrement(){
+         count--;
+     },
+     increment(){
+         count++;
+     }
+ };
+ }());
 
-// console.log(counting.value()); // 0
 
-// counting.increment();
+  console.log(counting.value()); // 0
 
-// counting.increment();
+  counting.increment();
 
-// counting.increment();
+  counting.increment();
 
-// console.log(counting.value()); // 3
+  counting.increment();
 
-// counting.decrement();
+  console.log(counting.value()); // 3
 
-// counting.decrement();
+  counting.decrement();
 
-// console.log(counting.value()); // 1
+    counting.decrement();
 
-// console.log(counting.value(100)); // 100
+  console.log(counting.value()); // 1
 
-// counting.decrement();
+  console.log(counting.value(100)); // 100
 
-// console.log(counting.value()); // 99
+  counting.decrement();
 
-// console.log(counting.value(200)); // 200
+  console.log(counting.value()); // 99
 
-// counting.increment();
+  console.log(counting.value(200)); // 200
 
-// console.log(counting.value()); // 201
+  counting.increment();
+
+  console.log(counting.value()); // 201
 
 /*
  * #3
  *
- * Создайте функцию myPow(a, b, myPrint). Внутри реализуйте рекурсию для подсчета результата возведения числа a в степень b.
- * функция myPrint(a, b, res) – глобальная функция, которая должна генерировать из параметров a, b, res строку вида 'a^b=res' и возвращать ее
- * myPrint() должна быть передана в myPow() как параметр и вызвана внутри как callback-функция
- * функция myPow() в качестве возвращаемого значения принимает результат myPrint()
+ * Создайте функцию myPow(a, b, myPrint). Внутри реализуйте 
+ * рекурсию для подсчета результата возведения числа a в степень b.
+ * функция myPrint(a, b, res) – глобальная функция, которая должна
+ *  генерировать из параметров a, b, res строку вида 'a^b=res' 
+ * и возвращать ее
+ * myPrint() должна быть передана в myPow() как параметр и 
+ * вызвана внутри как callback-функция
+ * функция myPow() в качестве возвращаемого значения принимает 
+ * результат myPrint()
  * Например:
  * console.log(myPow(3, 4, myPrint)); // 3^4=81
  * console.log(myPow(2, 3, myPrint)); // 2^3=8
  */
+let myPrint = (a, b, res) => `${a}^${b}=${res}`;
+let myPow = (a, b, callback) => {
+  let pow = (x, n) => {
+    if (n !== 1) return x *= pow(x, n - 1);
 
-//  console.log(myPow(3, 4, myPrint)); // 3^4=81
+    return x;
+  };
 
-// console.log(myPow(2, 3, myPrint)); // 2^3=8
+  return callback(a, b, pow(a, b));
+};
+
+
+//    function myPow (a,b,myPrint){
+//   return myPrint(a,b);
+//   };
+//   function myPrint(a,b,res){
+//       if (b!==1) {
+//           var res=a*=myPrint(a,b-1);
+//         return res;
+//       } else{ return a;}
+//     };
+
+
+var myPrint = (a, b, res) => `${a}^${b}=${res}`;
+var  myPow = function(a,b,callback) {
+    var res= function (a,b) {
+        if (b!==1) return a*=res(a,b-1);
+         return a;
+    };
+        return callback(a, b, res(a, b));
+    };
+    console.log(myPow(3, 4, myPrint)); // 3^4=81
+
+   console.log(myPow(2, 3, myPrint)); // 2^3=8
+
+ let nowYear = new Date().getFullYear();
+ let car={ 
+ model: 'Fabia',
+ engine: 1600,
+ name: 'Skoda',
+ year: 2008, 
+ get used (){  return this.year===nowYear ? 'new':'used'},
+ set used (value){ if  (value=== 'new' && this.year < nowYear) this.year = nowYear},
+ info: fullInfo,
+ };
+ console.log(car.info());
+
+ console.log(car.info());
+ let car2={ 
+ model: 'Rav4',
+ engine: 2200,
+ name: 'Toyota',
+ year: 2022,
+ //used: (car2.year===nowYear)? 'new':false ||'used';
+ info: fullInfo,
+ get used (){  return this.year===nowYear ? 'new':false ||'used'},
+ set used (value){ if  (value=== 'new' && this.year < nowYear) this.year = nowYear},
+ };
+
+ console.log(car2);
+
+ function fullInfo(){
+     return `${this.name} ${this.model}, ${this.engine}cc, year ${this.year}, ${this.used}`;
+ }
+ console.log(car2.info());
+ console.log(car.info());
+
 
 /*
  * #4
  *
- * Создайте несколько однотипных объектов для описания автомобиля. Соблюдайте следующие правила, используйте следующие поля:
- * имя объекта car – обязательно и необходимое для тестирования, дальнейшее именование объектов – на ваше усмотрение
+ * Создайте несколько однотипных объектов для описания автомобиля.
+ *  Соблюдайте следующие правила, используйте следующие поля:
+ * имя объекта car – обязательно и необходимое для тестирования,
+ *  дальнейшее именование объектов – на ваше усмотрение
  * car.engine – объем двигателя, числовое поле
  * car.model – модель авто, строка
  * car.name – бренд авто, строка
  * car.year – год выпуска, число
- * car.used – строка для описания состояния авто, допускаются значения 'used' и 'new'
+ * car.used – строка для описания состояния авто, допускаются 
+ * значения 'used' и 'new'
  *
  * #5
- *
- * Для созданных ранее объектов определите метод info(), используя ключевое слово this.
- * данный метод должен формировать и возвращать строку с полной информацией об автомобиле, например:
+ * Для созданных ранее объектов определите метод info(), используя 
+ * ключевое слово this.
+ * данный метод должен формировать и возвращать строку с полной 
+ * информацией об автомобиле, например:
  * Chevrolet Lacetti, 2000cc, year 2010, used
  * Infinite FX50 AWD, 5000cc, year 2019, new
- * пробелы, запятые, символы cc и текст – имеют значение и проверяются при тестировании кода
- *
+ * пробелы, запятые, символы cc и текст – имеют значение и 
+ * проверяются при тестировании кода
  * #6
  *
- * Для созданных ранее объектов измените свойство used, используя аксессоры (геттер и сеттер).
- * - используйте текущий год либо непосредственно в своем коде, либо с помощью глобальной переменной, например, yearNow
- * - если год выпуска автомобиля отличается от текущего года, геттер used должен выводить текст 'used'
- * - если год выпуска автомобиля совпадает с текущим годом, геттер used должен выводить текст 'new'
- * - если сеттеру used присвоено значение 'new', при этом года выпуска автомобиля отличается от текущего года,
- * - необходимо изменить год выпуска автомобиля, установив в качестве значения текущий год
- * - если сеттеру used присвоено значение 'used', ничего делать не нужно
+ * Для созданных ранее объектов измените свойство used, используя 
+ * аксессоры (геттер и сеттер).
+ * - используйте текущий год либо непосредственно в своем коде, 
+ * либо с помощью глобальной переменной, например, yearNow
+ * - если год выпуска автомобиля отличается от текущего года,
+ *  геттер used должен выводить текст 'used'
+ * - если год выпуска автомобиля совпадает с текущим годом, 
+ * геттер used должен выводить текст 'new'
+ * - если сеттеру used присвоено значение 'new', при этом года 
+ * выпуска автомобиля отличается от текущего года,
+ * - необходимо изменить год выпуска автомобиля, установив в 
+ * качестве значения текущий год
+ * - если сеттеру used присвоено значение 'used', ничего делать 
+ * не нужно
  */
 
-// let yearNow = new Date().getFullYear(); // получить текущий год как число
+// let yearNow = new Date().getFullYear(); // получить текущий
+ //год как число
 
-// console.log(car.info()); // Chevrolet Lacetti, 2000cc, year 2010, used
+  console.log(car.info()); // Chevrolet Lacetti, 2000cc, year
+  2010//, used
 
-// car.used = 'new';
+ car.used = 'new';
 
-// console.log(car.info()); // Chevrolet Lacetti, 2000cc, year 2019, new -- год изменен
+  console.log(car.info()); // Chevrolet Lacetti, 2000cc, year 2019, new -- год изменен
 
-// car.used = 'used';
+  car.used = 'used';
 
-// console.log(car.info()); // Chevrolet Lacetti, 2000cc, year 2019, new -- изменения не выполняются
+  console.log(car.info()); // Chevrolet Lacetti, 2000cc, year 2019, new -- изменения не выполняются
 
-// console.log(car2.info()); // Infinite FX50 AWD, 5000cc, year 2019, new
+  console.log(car2.info()); // Infinite FX50 AWD, 5000cc, year 2019, new
 
-// car.used = 'used';
+  car.used = 'used';
 
-// console.log(car2.info()); // Infinite FX50 AWD, 5000cc, year 2019, new -- изменения не выполняются
+  console.log(car2.info()); // Infinite FX50 AWD, 5000cc, year 2019, new -- изменения не выполняются
 
 /*
  * #7
  * Создайте функцию myMax(arr), которая в качестве параметра принимает
- * произвольный числовой массив и возвращает максимальное число из переданного ей массива.
+ * произвольный числовой массив и возвращает максимальное число из 
+ * переданного ей массива.
  * В реализации функции должен быть применен метод Math.max() и apply().
  */
 
-// let list = [12, 23, 100, 34, 56, 9, 233];
 
-// console.log(myMax(list)); // 233
+// var myMax = function(){
+//   var max = Math.max.apply(null,list);
+//   return max;
+// };
+//  let list = [12, 23, 100, 34, 56, 9, 233,455, 715];
+//  console.log(myMax(list)); // 233
+
+
+
+ var myMax = function(list){
+       var max = Math.max.apply(Math,list);
+        return max;
+      };
+       let list = [12, 23, 100, 34, 56, 9, 233,455,777];
+       console.log(myMax(list)); // 233
+    
+
+
 
 /*
  * #8
- *
+ * 
  * Создайте функцию myMul(a, b), которая будет умножать числа а и b, возвращая результат.
  */
 
 /*
  * создайте функции myDouble(n), которая принимает один параметр и  удваивает его.
- * Использовать умножение или другие математические операции внутри функции – запрещено, только bind() и myMul().
+ * Использовать умножение или другие математические операции внутри функции – запрещено, 
+ * только bind() и myMul().
  * Функция возвращает результат вычисления.
  */
 
-// console.log(myDouble(3)); // = myMul(2, 3) = 6
 
-// console.log(myDouble(4)); // = myMul(2, 4) = 8
+// function myMul(a,b){ return a*b};
+// console.log(myMul(2,5)); 
 
-// console.log(myDouble(5)); // = myMul(2, 5) = 10
+// function myDouble(n){
+//    var result=myMul.bind(n,2);
+//    return result;
+// };
 
-// аналогичным образом создайте функцию myTriple(n), которая утраивает принимающий параметр, возвращая результат.
 
-// console.log(myTriple(3)); // = myMul(3, 3) = 9
 
-// console.log(myTriple(4)); // = myMul(3, 4) = 12
+ function myMul(a,b){ return a*b};
+  console.log(myMul(2,5)); 
+ var myDouble = myMul.bind(null, 2);
 
-// console.log(myTriple(5)); // = myMul(3, 5) = 15
+ var myTriple = myMul.bind(null, 3);
+
+
+
+  console.log(myDouble(3)); // = myMul(2, 3) = 6
+
+  console.log(myDouble(4)); // = myMul(2, 4) = 8
+
+  console.log(myDouble(5)); // = myMul(2, 5) = 10
+
+// // аналогичным образом создайте функцию myTriple(n), 
+// //которая утраивает принимающий параметр, возвращая результат.
+
+//  console.log(myTriple(3)); // = myMul(3, 3) = 9
+
+//  console.log(myTriple(4)); // = myMul(3, 4) = 12
+
+//  console.log(myTriple(5)); // = myMul(3, 5) = 15
 
 /*
  * #9
@@ -175,10 +318,17 @@ console.log('Sample JavaScript #3 HW #17');
  * Любые условные операторы – запрещены и объекты.
  */
 
-// let notUniqNums = [1, 1, 2, 3, 4, 5, 6, 7];
+let notUniqNums = [1, 1, 2, 3, 4, 5, 6, 7];
+let notUniqStrings = ['Bob', 'Kate', 'Jhon', 'Tom', 'Jhon', 'Kate', 'Tom', 'Bob', 'Jhon', 'Tom'];
 
-// let notUniqStrings = ['Bob', 'Kate', 'Jhon', 'Tom', 'Jhon', 'Kate', 'Tom', 'Bob', 'Jhon', 'Tom'];
 
-// console.log(myUniq(notUniqNums));
+let myUniq = function (arrey){
+let set = new Set();
+arrey.forEach((value) => {set.add(value);
+});
+return set;
+};
 
-// console.log(myUniq(notUniqStrings));
+ console.log(myUniq(notUniqNums));
+
+ console.log(myUniq(notUniqStrings));
